e3439e2151bbba476567bc8528b137c7f5d8aa4c
Grzegorz Miśkiewicz <biuro@avatec.pl>
2023-02-27 18:15:51 +0100

    no message

--- database/migrations/2023_02_25_113714_create_github_table.php
+++ database/migrations/2023_02_25_113714_create_github_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('url');
-            $table->json();
+            $table->longText('stats');
             $table->timestamps();
         });
     }
